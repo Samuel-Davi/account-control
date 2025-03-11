36235897fe62b762de2d5404a574f3bab1db518a
Samuel Davi <samueldavi.0907@gmail.com>
2025-03-10 23:59:06 -0400

    correção de codigo

--- src/app/(pages)/(private)/dashboard/page.tsx
+++ src/app/(pages)/(private)/dashboard/page.tsx
@@ -1,9 +1,4 @@
 'use client'
-
-import { AuthContext } from "@/app/contexts/AuthContext"
-import { useContext, useEffect, useState } from "react"
-
-
 export default function Dashboard() {
 
   return (
